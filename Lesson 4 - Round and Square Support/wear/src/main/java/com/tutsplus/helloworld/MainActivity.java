b73212e9996102aad7676be776b61d84968d8c15
Paul Trebilcox <Paul@Spheros-MacBook-Pro-4.local>
2015-07-05 11:14:35 -0600

    lesson 4

---
+++ Lesson 4 - Round and Square Support/wear/src/main/java/com/tutsplus/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tutsplus.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
