ae02cf5eeb03435bba707301959c34d98495f566
Paul Trebilcox <Paul@Spheros-MacBook-Pro-4.local>
2015-07-19 19:28:53 -0600

    Lesson 11 - Stacked and Action Button notifications

---
+++ Lesson 11 - Stacked and Action Button Notifications/wear/src/main/java/com/tutsplus/helloworld/views/WearableListItemLayout.java
@@ -0,0 +1,50 @@
+package com.tutsplus.helloworld.views;
+
+import android.content.Context;
+import android.support.wearable.view.WearableListView;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.tutsplus.helloworld.R;
+import com.tutsplus.helloworld.activities.MainActivity;
+
+/**
+ * Created by Paul on 7/6/15.
+ */
+public class WearableListItemLayout extends LinearLayout implements WearableListView.OnCenterProximityListener {
+
+    private TextView mText;
+
+    public WearableListItemLayout(Context context) {
+        super(context);
+    }
+
+    public WearableListItemLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WearableListItemLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mText = (TextView) findViewById( R.id.text );
+    }
+
+    @Override
+    public void onCenterPosition(boolean b) {
+        if( mText != null && !MainActivity.isAmbientMode ) {
+            mText.setTextColor( getResources().getColor( android.R.color.black ) );
+        }
+    }
+
+    @Override
+    public void onNonCenterPosition(boolean b) {
+        if( mText != null && !MainActivity.isAmbientMode ) {
+            mText.setTextColor( getResources().getColor( android.R.color.darker_gray ) );
+        }
+    }
+}
